b7c8e18acb4618337b2b955472017742f2473901
Uday Singh Solanki <37234643+udaysolanki4@users.noreply.github.com>
2019-09-15 14:08:32 +0530

    Create conf2.py

---
+++ conf2.py
@@ -0,0 +1,4 @@
+API_KEY = "XXXX"        //Bolt Cloud
+API KeyDEVICE_ID = "BOLTXXXX"    //Device ID of the Bolt Module
+FRAME_SIZE = 10        //Frame Size for Z score analysis
+MUL_FACTOR = 4        //Multiplication factor for Z score analysis
